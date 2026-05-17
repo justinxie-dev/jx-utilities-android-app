29b86fa8c7f4fb318ad44388dd0737caa544fcd6
Justin Xie <justin_xie@outlook.com>
2026-05-17 17:53:03 -0500

    feat: add AppDatabase to hold the actual database and serves as app's main access point to the persisted data

---
+++ JxUtilities/app/src/main/java/io/github/justinxie_dev/jxutilities/AppDatabase.java
@@ -0,0 +1,18 @@
+package io.github.justinxie_dev.jxutilities;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+// Reference: https://developer.android.com/training/data-storage/room (Database section)
+@Database(entities = {CalculatorDataEntity.class, ConverterDataEntity.class, MapsDataEntity.class, GradeCalculatorDataEntity.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract CalculatorDao calculatorDao();
+
+    public abstract ConverterDao converterDao();
+
+    public abstract MapsDao mapsDao();
+
+    public abstract GradeCalculatorDao gradeCalculatorDao();
+
+}
+
